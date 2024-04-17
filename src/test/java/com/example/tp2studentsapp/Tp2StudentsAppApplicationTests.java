94c3f34b3d412c48a194b56cec3b9c685d22d2a6
Aymane Tajani <aymanetajani01@gmail.com>
2024-04-17 09:31:36 +0100

    first commit

---
+++ src/test/java/com/example/tp2studentsapp/Tp2StudentsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp2studentsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2StudentsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
